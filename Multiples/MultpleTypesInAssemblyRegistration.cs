cb4375eb9e2b1cba162c97e5f190ce342715ee6c
Chris Sampson <csampson@appliedsystems.com>
2021-09-28 14:19:41 +0100

    Added missing type extensions that were part of relfection originally

--- Multiples/MultpleTypesInAssemblyRegistration.cs
+++ Multiples/MultpleTypesInAssemblyRegistration.cs
@@ -13,5 +13,10 @@ namespace SystemDot.Ioc.Multiples
 
         public MultpleTypesInAssemblyFilteredRegistration AllTypes() =>
             new MultpleTypesInAssemblyFilteredRegistration(container, typeof(TAssemblyOf).GetTypesInAssembly().WhereNormalConcrete());
+
+        public MultpleTypesInAssemblyFilteredRegistration ThatImplementType<T>() => new MultpleTypesInAssemblyFilteredRegistration(this.container, typeof(TAssemblyOf).GetTypesInAssembly().WhereNormalConcrete().WhereImplements<T>());
+
+        public MultpleTypesInAssemblyFilteredRegistration ThatImplementOpenType(Type openType) =>
+            new MultpleTypesInAssemblyFilteredRegistration(container, typeof(TAssemblyOf).GetTypesInAssembly().WhereNormalConcrete().WhereImplementsOpenType(openType));
     }
 }
--- Multiples/TypeExtensions.cs
+++ Multiples/TypeExtensions.cs
@@ -5,8 +5,10 @@ using System.Reflection;
 
 namespace SystemDot.Ioc.Multiples
 {
-    internal static class TypeExtensions
+    public static class TypeExtensions
     {
+        public static Assembly GetAssembly(this Type type) => type.GetTypeInfo().Assembly;
+
         public static IEnumerable<Type> GetTypesInAssembly(this Type type)
         {
             return type.GetTypeInfo().Assembly.ExportedTypes;
@@ -33,5 +35,61 @@ namespace SystemDot.Ioc.Multiples
         {
             return type.GetAssembly().FullName == typeof(T).GetAssembly().FullName;
         }
+    
+        public static IEnumerable<Type> ThatImplement<TType>(this IEnumerable<Type> types) => types.WhereNormalConcrete().WhereImplements<TType>();
+
+        public static IEnumerable<Type> WhereImplements<TImplemented>(
+          this IEnumerable<Type> types)
+        {
+            return types.Where(t => t.GetNonBaseInterfaces().Contains(typeof(TImplemented)) || t.GetBaseInterfaces().Contains(typeof(TImplemented)));
+        }
+
+        public static IEnumerable<Type> GetNonBaseInterfaces(this Type type)
+        {
+            IEnumerable<Type> baseInterfaces = type.GetBaseInterfaces();
+            return type.GetInterfaces().Where(t => !baseInterfaces.Contains(t));
+        }
+
+        private static IEnumerable<Type> GetBaseInterfaces(this Type type)
+        {
+            var typeList = new List<Type>();
+            Type baseType = type.GetTypeInfo().BaseType;
+            if (baseType == typeof(MemberInfo))
+            {
+                return typeList;
+            }
+
+            while (baseType != null)
+            {
+                typeList.AddRange(baseType.GetInterfaces());
+                baseType = baseType.GetTypeInfo().BaseType;
+                if (baseType == typeof(MemberInfo))
+                    return typeList;
+            }
+
+            return typeList;
+        }
+
+        public static Type[] GetInterfaces(this Type type) => type.GetTypeInfo().ImplementedInterfaces.ToArray<Type>();
+
+        public static IEnumerable<Type> WhereNormalConcrete(this IEnumerable<Type> types) => types.WhereNonAbstract().WhereNonGeneric().WhereConcrete();
+
+        private static IEnumerable<Type> WhereNonAbstract(this IEnumerable<Type> types) => types.Where(t => !t.IsAbstract());
+
+        private static bool IsAbstract(this Type type) => type.GetTypeInfo().IsAbstract;
+
+        private static IEnumerable<Type> WhereConcrete(this IEnumerable<Type> types) => types.Where(IsConcrete);
+
+        public static bool IsNormalConcrete(this Type type) => !type.IsAbstract() && !type.IsGeneric() && type.IsConcrete();
+
+        private static bool IsConcrete(this Type type) => !type.GetTypeInfo().IsInterface && type.GetTypeInfo().IsClass;
+
+        private static IEnumerable<Type> WhereNonGeneric(this IEnumerable<Type> types) => types.Where(t => !t.IsGeneric());
+
+        private static bool IsGeneric(this Type type) => type.GetTypeInfo().ContainsGenericParameters;
+
+        public static MethodInfo GetMethod(this Type type, string methodName, Type[] args) => type.GetRuntimeMethod(methodName, args);
+
+        public static IEnumerable<MethodInfo> GetMethods(this Type type) => type.GetRuntimeMethods();
     }
 }
